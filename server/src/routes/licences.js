dc4ff0c9dd866753b64c2e4d8081171dd0d96dcf
Krajcara <162749751+Krajcara@users.noreply.github.com>
2026-07-29 15:43:16 +0200

    Update licences.js

--- server/src/routes/licences.js
+++ server/src/routes/licences.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router  = express.Router();
+const { encrypt, decrypt } = require('../services/encryptionService');
 const db      = require('../db/database');
 const { requireAuth } = require('../middleware/auth');
 const { writeAuditLog } = require('../middleware/audit');
