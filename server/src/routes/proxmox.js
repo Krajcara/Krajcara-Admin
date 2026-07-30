2de917e21d7f5b6cca478f83503a351c230b3cea
Krajcara <162749751+Krajcara@users.noreply.github.com>
2026-07-30 10:08:27 +0200

    Update proxmox.js

--- server/src/routes/proxmox.js
+++ server/src/routes/proxmox.js
@@ -1,6 +1,5 @@
 'use strict';
 const express = require('express');
-const { decrypt } = require('../services/encryptionService');
 const router  = express.Router();
 const axios   = require('axios');
 const https   = require('https');
@@ -16,7 +15,7 @@ function getConfig() {
   const url     = db.prepare("SELECT value FROM settings WHERE key='proxmox_url'").get()?.value;
   const user    = db.prepare("SELECT value FROM settings WHERE key='proxmox_user'").get()?.value || 'root@pam';
   const tokenId = db.prepare("SELECT value FROM settings WHERE key='proxmox_token_id'").get()?.value || '';
-  const secret  = decrypt(db.prepare("SELECT value FROM settings WHERE key='proxmox_api_token'").get()?.value) || '';
+  const secret  = db.prepare("SELECT value FROM settings WHERE key='proxmox_api_token'".get()?.value) || '';
   let token;
   if (secret.includes('!') && secret.includes('=')) token = secret;
   else if (tokenId && secret) token = `${user}!${tokenId}=${secret}`;
